b35c90486cef55d2bdcfaae21e4ee0b0582771cf
natthasuda <nutsuk246@ihmeducation.se>
2024-09-19 14:23:49 +0200

    Implementing buttons

--- RollingDice/js/app.js
+++ RollingDice/js/app.js
@@ -5,14 +5,20 @@ let button0 = document.getElementById("button0");
 //Texts
 let playerRollText = document.getElementById("playerRollText");
 let aiRollText = document.getElementById("aiRollText");
+let resultText = document.getElementById("resultText");
 
 //Data
 let playerRoll = 0;
 let aiRoll = 0;
 
+//Score
+let playerScore = 0;
+let aiScore = 0;
+
 //Process
 button0.addEventListener("click", function () {
   playerRoll = getRandomNumberOneToSix();
+  showPlayerRollResult();
 
 });
 
